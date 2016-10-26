4c40d10e941e06e2057b2eea9fb912777f88a5dd
Chris Norman <chris.norman2@googlemail.com>
2016-10-26 19:35:29 +0100

    Added log format to parser.

--- default_argparse/__init__.py
+++ default_argparse/__init__.py
@@ -4,3 +4,4 @@ from argparse import ArgumentParser, FileType
 parser = ArgumentParser()
 parser.add_argument('-l', '--log-file', default = sys.stdout, type = FileType('w'), help = 'Log file')
 parser.add_argument('-L', '--log-level', default = 'INFO', choices = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help = 'Logging level')
+parser.add_argument('-F', '--log-format', default = '[%(asctime)s] %(name)s.%(levelname)s: %(message)s', help = 'The logging format')
--- setup.py
+++ setup.py
@@ -1,15 +1,5 @@
 from setuptools import setup, find_packages
 
 setup(
- name = 'default_argparse',
- version = '0.0.0',
- description = 'My default argument parser.',
- url = 'http://github.com/chrisnorman7/default_argparse.git',
- author = 'Chris Norman',
- author_email = 'chris.norman2@googlemail.com',
- license = 'MPL',
- packages = find_packages(),
- zip_safe = False,
- long_description_markdown_filename = 'README.md',
- setup_requires = ['setuptools-markdown'],
+    setup_requires = ['pbr', 'setuptools'],
 )
